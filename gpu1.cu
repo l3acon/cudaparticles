bf9e56898d3daa70127ced36fab84b7516944357
matt <l3acon@gmail.com>
2014-04-16 21:54:18 -0600

    updating

--- gpu1.cu
+++ gpu1.cu
@@ -10,7 +10,22 @@
 #define MAX_PARTICLES_PER_BIN 4
 
 
-__global__ void calBin()
+__global__ void initBins(int n)
+{
+	//	number of particles i neach bin
+	//	initialilzed to zero each step
+	int* binCounters = (int*) a;
+
+	//	indicies of each particle in a particular bin
+	//	only has room for MAX_PARTIVLES_PER_BIN
+	int* particlesInBin = (int*) binCounters[n];
+	
+	//	the size of numParticlesInBin is:
+	//	NUM_PARTICLES * MAX_PARTICLES_PER_BIN
+	int* unused = (int*) numParticlesInBin[n*MAX_PARTICLES_PER_BIN]; 
+}
+
+__device__ int2 calBin( float2 pos)
 {
 	int2 binPos;
 	binPos.x = floor(pos.x/PARTICLE_SIZE);
@@ -18,26 +33,20 @@ __global__ void calBin()
 	return binPos;
 }
 
-
-__device__ void apply_force_gpu(particle_t &particle, particle_t &neighbor)
+__global__ void updateBins( particle_t *particles, int n )
 {
-  double dx = neighbor.x - particle.x;
-  double dy = neighbor.y - particle.y;
-  double r2 = dx * dx + dy * dy;
-  if( r2 > cutoff*cutoff )
-      return;
-  //r2 = fmax( r2, min_r*min_r );
-  r2 = (r2 > min_r*min_r) ? r2 : min_r*min_r;
-  double r = sqrt( r2 );
+  int tid = threadIdx.x + blockIdx.x * blockDim.x;
+  if(tid >= n) return;
+	
+	int2 bi = calBin(particles[tid].pos);	//	calculate bin index
+	atomicAdd(&binCounters[bi.x*bi.y],1)
+	if(binCounters[bi.x*bi.y] >= MAX_PARTICLES_PER_BIN)
+		return;	//	probably do something else for error
 
-  //
-  //  very simple short-range repulsive force
-  //
-  double coef = ( 1 - cutoff / r ) / r2 / mass;
-  particle.ax += coef * dx;
-  particle.ay += coef * dy;
-
+	//	add our particle index to the bin	
+	atomicAdd(&particlesInBin[binCounters[bi.x*bi.y]], tid);
 }
+
 
 __global__ void compute_forces_gpu(particle_t * particles, int n)
 {
@@ -84,6 +93,25 @@ __global__ void move_gpu (particle_t * particles, int n, double size)
 
 }
 
+__device__ void apply_force_gpu(particle_t &particle, particle_t &neighbor)
+{
+  double dx = neighbor.x - particle.x;
+  double dy = neighbor.y - particle.y;
+  double r2 = dx * dx + dy * dy;
+  if( r2 > cutoff*cutoff )
+      return;
+  //r2 = fmax( r2, min_r*min_r );
+  r2 = (r2 > min_r*min_r) ? r2 : min_r*min_r;
+  double r = sqrt( r2 );
+
+  //
+  //  very simple short-range repulsive force
+  //
+  double coef = ( 1 - cutoff / r ) / r2 / mass;
+  particle.ax += coef * dx;
+  particle.ay += coef * dy;
+
+}
 int main( int argc, char **argv )
 {    
     // This takes a few seconds to initialize the runtime
@@ -112,8 +140,10 @@ int main( int argc, char **argv )
     particle_t * d_particles;
     cudaMalloc((void **) &d_particles, n * sizeof(particle_t));
 		set_size( n );
+		
+		//	use this as an address because it makes sense to me
+		extern __shared__ int a[];
+		//	extern __shared__ int binIndicies[];
 
-		extern __shared__ int binCounters[];
-		extern __shared__ int binIndicies[];
 
 }
